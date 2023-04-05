ee017a4542d2382fe6e3635c85bbbae2e2403feb
Rafael Alves Ferreira Lima <rafael.lima@vitaebrasil.com.br>
2023-04-05 17:09:17 -0300

    First Commit

---
+++ Javinha/src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor do primeiro cateto: ");
+        Double cateto1 = sc.nextDouble();
+
+        System.out.print("Digite o valor do segundo cateto: ");
+        Double cateto2 = sc.nextDouble();
+        
+        Double hipotenusa = (double) (Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+
+        System.out.println(hipotenusa.toString().replace(".", ","));
+
+        sc.close();
+    }
+}
